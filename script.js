0a234c45a5a7afc0bf3a8e7a65fc430763a12c73
Mhingzzz <nathaphong_p@cmu.ac.th>
2022-08-04 14:15:48 +0700

    F

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-async function callApi() {
-  const resp = await axios.get("https://randomuser.me/api/");
+const loadingCard = document.getElementById("div-loading-card");
+const imgPerson = document.querySelector("img-profile");
+const userCard = document.getElementById("div-user-card");
+
+const pName = document.getElementById("p-name");
+const pAddress = document.getElementById("p-address");
+const pEmail = document.getElementById("p-email");
+
+const btnRandom = document.getElementById("btn-random");
+
+start();
+
+function start() {
+	callApi();
+	loadingCard.display.style = "none";
 }
+
+async function callApi() {
+	const resp = await axios.get("https://randomuser.me/api/");
+
+	const data = resp.data.results[0];
+	resp.onload = () => {
+		loadingCard.display.style = "";
+		imgPerson.src = resp.data.picture;
+		pName.innerText = resp.data.name;
+		pEmail.innerText = resp.data.email;
+		pAddress.innerText = resp.data.a;
+	};
+}
+
+btnRandom.onclick = async () => {
+	btnRandom.disable = true;
+	callApi();
+	pName.innerText = resp.data.name;
+	pEmail.innerText = resp.data.email;
+};
